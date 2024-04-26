dce7c4545a6f68d9e909b141f00fd26a79c5a766
Tony Sharples <tonysharples22@me.com>
2024-04-26 13:59:07 +0100

    Add button for picking and displaying a random name from the list

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -10,9 +10,12 @@ import SwiftUI
 struct ContentView: View {
     @State private var names: [String] = ["Tony", "Raimy", "Lewis", "Tali", "Mabel"]
     @State private var nameToAdd = ""
+    @State private var pickedName = ""
     
     var body: some View {
         VStack {
+            Text(pickedName.isEmpty ? " " : pickedName)
+            
             List {
                 ForEach(names, id: \.self) { name in
                     Text(name)
@@ -27,6 +30,14 @@ struct ContentView: View {
                         nameToAdd = ""
                     }
                 }
+            
+            Button("Pick Random Name") {
+                if let randomName = names.randomElement() {
+                    pickedName = randomName
+                } else {
+                    pickedName = ""
+                }
+            }
         }
         .padding()
     }
